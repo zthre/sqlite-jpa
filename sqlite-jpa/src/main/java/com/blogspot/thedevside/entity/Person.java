ec2c47dff00b21a9e0024691f9fc904d5b1f8a30
Wil Faver <wilfa4243@hotmail.com>
2019-06-26 11:57:27 -0500

    Nuevo cambio

--- sqlite-jpa/src/main/java/com/blogspot/thedevside/entity/Person.java
+++ sqlite-jpa/src/main/java/com/blogspot/thedevside/entity/Person.java
@@ -23,7 +23,7 @@ public class Person implements Serializable {
 	private Integer id; 
 	private String name;
 	private Integer age;
-
+	private String dress;
 	public Integer getId() {
 		return id;
 	}
